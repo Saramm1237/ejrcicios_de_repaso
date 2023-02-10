3dbaa9911673eee004d3142af3d77a9892f100a7
sara medina molina <smedina298@soyudemedellin.edu.co>
2023-02-09 21:31:12 -0500

    ejercicio 13 realizado

---
+++ ejercicios de repaso/ejercicio_13.py
@@ -0,0 +1,7 @@
+a = int(input("ingrese primer numero: "))
+b = int(input("ingrese segundo numero: "))
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a/b
+print(f"la suma es = {suma} la resta es = {resta} la multiplicacion es = {multiplicacion} la division es = {division}")
