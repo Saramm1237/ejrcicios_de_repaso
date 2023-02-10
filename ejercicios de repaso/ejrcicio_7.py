1c095623e995d7038f6a90429d325849a11fe970
sara medina molina <smedina298@soyudemedellin.edu.co>
2023-02-09 21:25:32 -0500

    ejercicio 7 realizado

---
+++ ejercicios de repaso/ejrcicio_7.py
@@ -0,0 +1,7 @@
+def mayorMenor(l):
+  mayor = max(l)
+  print(f"el numero mayor de su lista es: {mayor}")
+  menor = min(l)
+  print(f"el numero menor de su lista es: {menor}")
+
+mayorMenor([1, 3, 5, 4])
